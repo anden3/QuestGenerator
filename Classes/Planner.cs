3bc39cc77705c1cea995968cff24ab86f9ec0808
André Vennberg <andre.vennberg@gmail.com>
2020-02-20 17:14:35 +0100

    Working on getting planner integrated with C#.

---
+++ Classes/Planner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace QuestGenerator
+{
+    struct action_t
+    {
+        byte[] op;
+    }
+
+    struct result_t
+    {
+        byte[] problem;
+        float timeTaken;
+        int length;
+        int cost;
+
+        action_t[] actions;
+    }
+
+    class Planner
+    {
+        [DllImport("hsp2.dll", EntryPoint = "plan", CharSet = CharSet.Auto)]
+        internal static extern int plan(int argc, byte[][] argv, result_t result);
+    }
+}
